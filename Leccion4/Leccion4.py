aa35de825e523db11949834d787e6eaf7d313335
EvaGGCC <eva78985@hotmail.com>
2022-01-25 23:41:46 +0000

    Maximol lista con bucle for

--- Leccion4/Leccion4.py
+++ Leccion4/Leccion4.py
@@ -5,6 +5,10 @@ import pdb
 #Importamos la funcion reduce. Obtiene un solo resultado de una lista
 from functools import reduce
 
+"""
+    Calculo valores maximos de una lista
+"""
+
 # -----> FUNCION MAXIMO
 
 # ... Método compresion de listas
@@ -18,4 +22,26 @@ def max_compresion(lista):
         i: cada una de las sublistas
     """
     maximos=[max(i) for i in lista]
+    return maximos
+
+
+# -----> MEDIANTE BUCLES FOR
+
+# ... Metodo convencional
+def maximo_convencional(lista):
+    """
+    Calculo del maximo de lista de listas mediante bucles convencionales.
+    No se puede trasladar como tal a compresion de listas, uno de los bucles
+    no funciona mediante listas sino con variables
+    Contador:
+        i: cada una de las sublistas
+        j: cada uno de las posiciones de la sublista
+    """
+    maximos=[]
+    for i in lista:
+        m=0
+        for j in range(len(i)):
+            if i[j] > m:
+                m = i[j]
+        maximos.append(m)
     return maximos
