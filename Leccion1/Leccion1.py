1cdc36936a5136669b12413996a75fd54ba6923f
EvaGGCC <eva78985@hotmail.com>
2022-01-26 00:20:47 +0000

    Comprobacion existencia documento

--- Leccion1/Leccion1.py
+++ Leccion1/Leccion1.py
@@ -1,1 +1,26 @@
-# Prueba 1
+# Eva Gonzalez Correa
+
+
+# Importamos modulos
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# Abrimo fichero y comprobamos su existencia
+#Defino una variable global, que se indicara en la función y se actualizara
+fichero=0
+def abrir_fichero(documento):
+    # Indicamos que fichero es variable global
+    global fichero
+    try:
+        # Abrimos el documento csv
+        with open(documento) as csv:
+            # Añadimos el documento a la variable global definida
+			# Tenemos en cuenta el delimitador de las columnas en este caso.
+            fichero = pd.read_csv(csv, delimiter = "\t", header = 0)
+            respuesta = "El archivo existe"
+    # En caso de que no exista generamos una excepcion
+    except FileNotFoundError as e:
+        respuesta = "El archivo no existe"
+        print(respuesta)
+    return respuesta
